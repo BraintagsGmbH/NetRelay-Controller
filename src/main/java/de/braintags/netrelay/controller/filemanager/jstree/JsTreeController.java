ff6598e3a7c1060f8c5c4c5ebd7b57878840d811
Michael Remme <mremme@braintags.de>
2017-02-13 10:52:20 +0100

    added documentation

--- src/main/java/de/braintags/netrelay/controller/filemanager/jstree/JsTreeController.java
+++ src/main/java/de/braintags/netrelay/controller/filemanager/jstree/JsTreeController.java
@@ -19,6 +19,44 @@ import de.braintags.netrelay.controller.filemanager.elfinder.io.impl.VertxVolume
 import io.vertx.core.file.FileSystem;
 
 /**
+ * This controller builds the api to support the web base tree manager https://www.jstree.com/
+ * It extends {@link ElFinderController} and thus supports the complete api of ElFinder and sends responses like needed
+ * by JSTree
+ * 
+ * 
+ * <br/>
+ * Config-Parameter:<br/>
+ * possible parameters, which are read from the configuration
+ * <UL>
+ * <LI>rootDirectories - defines directories to be used in the form of VolumeId:rootDirectory.
+ * Note: the path of the defined root directory in the example above defines the name of the volume before the colon,
+ * like it is displayed in the elfinder component.
+ * 
+ * </UL>
+ * <br>
+ * 
+ * Request-Parameter:<br/>
+ * the request parameters are defined by the required api like described at
+ * https://github.com/Studio-42/elFinder/wiki/Client-Server-API-2.1
+ * <br/>
+ * 
+ * Result-Parameter:<br/>
+ * results are sent like required by the JSTree component for the suitable command
+ * 
+ * Example configuration:<br/>
+ * 
+ * <pre>
+ * 
+   {
+      "name" : "JsTreeController",
+      "controller" : "de.braintags.netrelay.controller.filemanager.jstree.JsTreeController",
+      "routes" : [ "/fileManager/api/jstree"  ],
+      "handlerProperties" : { 
+        "rootDirectories" : "ROOTVOLUME:webroot"
+      }
+    }
+ * </pre>
+ * 
  * 
  * 
  * @author Michael Remme
