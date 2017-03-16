1f4202b40ed62de14ce6695ba2fb85605096deeb
Stefan Schmitt <stefan.schmitt@braintags.de>
2017-03-16 17:57:56 +0100

    WIP

---
+++ src/main/java/de/braintags/netrelay/controller/querypool/exceptions/IndexedFieldNotFoundException.java
@@ -0,0 +1,20 @@
+package de.braintags.netrelay.controller.querypool.exceptions;
+
+/**
+ * 
+ * 
+ * @author sschmitt
+ * 
+ */
+public class IndexedFieldNotFoundException extends QueryPoolException {
+
+  public IndexedFieldNotFoundException(String message, Exception cause) {
+    super(message, cause);
+  }
+
+  /**
+   * Comment for <code>serialVersionUID</code>
+   */
+  private static final long serialVersionUID = -8343659821242968318L;
+
+}
