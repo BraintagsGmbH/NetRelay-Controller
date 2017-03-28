9d870c7938f3911b2cf25617e78ce002a76c313c
sschmittBt <stefan.schmitt@braintags.de>
2017-03-28 15:11:47 +0200

    Revert "WIP:shopstarter-fixes"

--- src/main/java/de/braintags/netrelay/templateengine/thymeleaf/MultiPathResourceResolver.java
+++ src/main/java/de/braintags/netrelay/templateengine/thymeleaf/MultiPathResourceResolver.java
@@ -44,7 +44,7 @@ public class MultiPathResourceResolver extends ResourceTemplateResolver {
    *
    */
   public MultiPathResourceResolver(Vertx vertx, String templateDirectory) {
-    super(vertx, templateDirectory);
+    super(vertx);
     this.templateDirectory = templateDirectory;
     setName("braintags/Thymeleaf3");
   }
@@ -66,16 +66,19 @@ public class MultiPathResourceResolver extends ResourceTemplateResolver {
     }
     String file = template;
     if (!vertx.fileSystem().existsBlocking(file)) {
-      String cpFile = templateDirectory + file;
+      if (file.startsWith(templateDirectory)) {
+        String cpFile = file.substring(templateDirectory.length());
         if (cpFile.startsWith("/")) {
-        cpFile = cpFile.substring(1);
+          cpFile = cpFile.substring(1);
         }
         if (vertx.fileSystem().existsBlocking(cpFile)) {
           file = cpFile;
         } else {
           throw new NoSuchFileException(String.format(NOT_FOUND, file, cpFile));
         }
-
+      } else {
+        throw new IllegalArgumentException("Wrong requested file, path does not start with template directory");
+      }
     }
     pathLookup.put(template, file);
     return file;
